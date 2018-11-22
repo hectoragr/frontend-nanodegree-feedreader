a1e4edb136e99b73106a5c0d626eafbe37716c30
Héctor Gómez Reyes <hector.agr@gmail.com>
2018-11-22 12:18:08 -0800

    Feature: Split URL and Name tests in two
    Comply with Udacity review guidelines

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,36 +21,42 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-        it('All feeds have a well defined name and url', function(){
+        it('have a well defined name', () => {
             // For each element in allFeeds
             for(let feed of allFeeds) {
                 expect(feed.name).toBeDefined(); // Assert that name attribute is defined
+                expect(feed.url.trim().length).not.toBe(0); // Assert that url's lenght after trimming, is gt than 0
+            }
+        });
+
+        it('have a well defined url', () => {
+           // For each element in allFeeds
+            for(let feed of allFeeds) {
                 expect(feed.url).toBeDefined(); // Assert that url attribute is defined
                 expect(feed.url.trim().length).not.toBe(0); // Assert that url's lenght after trimming, is gt than 0
-                expect(feed.name.trim().length).not.toBe(0); // Aassert that name's lenght after trimmign is gt than 0
             }
         });
 
     });
 
     // Suite to test Menu behaviour
-    describe('The Menu', function(){
+    describe('The Menu', () => {
         const menu = document.querySelector('.slide-menu'); // Get menu element
         const menuLink = document.querySelector('.menu-icon-link'); // Get hamburger menu
 
         // Menu is hidden when loaded
-        it('The Menu is hidden by default', function(){
+        it('is hidden by default', () => {
             expect(menu).toBeDefined(); // Menu element is defined
             expect(menu.parentElement.classList.contains('menu-hidden')).toBe(true); // Menu contains class that hides it
         });
 
         // Menu is shown when menu icon is clicked, and hidden when the latter is clicked again
-        it('The Menu is shown when menu icon is clicked', function(){
+        it('is shown when menu icon is clicked', () => {
             expect(menu).toBeDefined();
             menuLink.click();
             expect(menu.parentElement.classList.contains('menu-hidden')).toBe(false);
@@ -60,17 +66,17 @@ $(function() {
     });
 
     // When we load the feed for the first time, there are entries
-    describe('Initial Entries', function(){
+    describe('Initial Entries', () => {
         /*
         * Given our call is asynchronous, we want to verify that the done callback is returned form
         * loadFeed when it completes, and we can then call our test which dependes on it
         */
 
-        beforeEach(function(done){
+        beforeEach((done) => {
             loadFeed(0, done);
         });
 
-        it('When we call the loadFeed function, there is at least one entry in the feed', function(done){
+        it('when we call the loadFeed function, there is at least one entry in the feed', (done) => {
             const feedContainer = document.querySelector('.feed'); // feed container
             for (const entryLink of feedContainer.children){ // for each link element in feed Container
                 const entry = entryLink.querySelector('.entry'); // retrieve article element
@@ -88,18 +94,18 @@ $(function() {
     });
 
     // When we load a initially load the feed, and we load a new one. They are different
-    describe('New Feed Selection', function(){
+    describe('New Feed Selection', () => {
 
         let firstFeed; // variable to hold initialFeed content
 
-        beforeEach(function(done){
-            loadFeed(0, function(){
+        beforeEach((done) => {
+            loadFeed(0, () => {
                 firstFeed = document.querySelector('.feed').innerHTML; // we save first Feed
                 loadFeed(1, done); // we make async call to change content
             });
         });
 
-        it('When a new feed is loaded, the content actually changes.', function(done){
+        it('when a new feed is loaded, the content actually changes.', (done) => {
             let secondFeed = document.querySelector('.feed').innerHTML; // We load second content
             expect(firstFeed.trim().length).not.toBe(0); // There was content loaded first
             expect(secondFeed.trim().length).not.toBe(0); // There is a second content
@@ -107,7 +113,7 @@ $(function() {
             done();
         });
 
-        afterEach(function(done){
+        afterEach((done) => {
             loadFeed(0, done); // We return to original content
         });
     });
